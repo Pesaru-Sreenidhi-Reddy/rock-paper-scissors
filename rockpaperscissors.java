2cc666f5185d01f20e90d9ea22e5ed3d0c2de2b7
Pesaru Sreenidhi Reddy <82439346+Pesaru-Sreenidhi-Reddy@users.noreply.github.com>
2023-07-31 17:59:39 +0530

    Update rockpaperscissors.java

--- rockpaperscissors.java
+++ rockpaperscissors.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.Random;
-public class Rockpaperscissors
+public class rockpaperscissors
 {
 	public static void main(String[] args) {
 	    Random rand = new Random();
